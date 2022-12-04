6043703b0355130b7d5df2a42792bf7840e8ffd7
Maciej <maciej.beresewicz@gmail.com>
2022-12-04 21:57:08 +0100

    function for day4

--- day4.jl
+++ day4.jl
@@ -9,23 +9,23 @@ example = "2-4,6-8
 6-6,4-6
 2-6,4-8"
 
-df = CSV.read(IOBuffer(example), DataFrame; header=["a", "b"], types = [String, String]) 
-df.a = eval.(Meta.parse.(replace.(df.a, "-" => ":")))
-df.b = eval.(Meta.parse.(replace.(df.b, "-" => ":")))
-df.check_ab = issubset.(df.a, df.b)
-df.check_ba = issubset.(df.b, df.a)
-df.flag = df.check_ab .| df.check_ba
-sum(df.flag)
+function check_subset(file, subset=true)
+    df = CSV.read(file, DataFrame; header=["a", "b"], types = [String, String]) 
+    df.a = eval.(Meta.parse.(replace.(df.a, "-" => ":")))
+    df.b = eval.(Meta.parse.(replace.(df.b, "-" => ":")))
+    df.check_ab = issubset.(df.a, df.b)
+    df.check_ba = issubset.(df.b, df.a)
+    df.flag = df.check_ab .| df.check_ba
+    df_full.check_inter = length.(intersect.(df_full.a, df_full.b)) .>0
 
-## 
-df_full = CSV.read(input, DataFrame; header=["a", "b"], types = [String, String]) 
-df_full.a = eval.(Meta.parse.(replace.(df_full.a, "-" => ":")))
-df_full.b = eval.(Meta.parse.(replace.(df_full.b, "-" => ":")))
-df_full.check_ab = issubset.(df_full.a, df_full.b)
-df_full.check_ba = issubset.(df_full.b, df_full.a)
-df_full.flag = df_full.check_ab .| df_full.check_ba
-sum(df_full.flag)
+    if subset == true
+        return sum(df.flag)
+    else
+        return sum(df_full.check_inter)
+    end
+end
 
-## part 2
-df_full.check_inter = length.(intersect.(df_full.a, df_full.b)) .>0
-sum(df_full.check_inter)
+
+check_subset(IOBuffer(example))
+check_subset(input)
+check_subset(input, false)
